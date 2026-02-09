075337d1e4ad5798000f3ad065a3af768468ae86
Its-Shubham_0246 <shubhammaurya2406@gmail.com>
2026-02-09 23:17:05 +0530

    first commit

---
+++ JavaScriptRefresher/app.js
@@ -0,0 +1,120 @@
+// import apiKey from "./util.js"
+// import {apiKey,api} from "./util.js"
+// import {apiKey as content} from "./util.js"
+// import * as util from "./util.js"
+// console.log(content)
+
+// let userMessage = "hello"
+// console.log(userMessage)
+// 
+// function greet(){
+//  console.log("hello shubham");
+// }
+// greet()
+
+// export default (userName,message) => {
+//  return userName + message;
+// };
+// 
+// const user = {
+//  name : "Shubham",
+//  age : 19,
+//  greet(){
+//   console.log(this.age)
+//  }
+// };
+// console.log(user)
+// console.log(user.name)
+// user.greet()
+
+// class User{
+//  constructer(name,age){
+//   this.name = name;
+//   this.age = age;
+//  }
+//  greet(){
+//   console.log("hi")
+//  }
+// }
+// const user1 = new User("Shubham",20);
+// user1.greet()
+// 
+// const hobbies = ["Singing","Cooking","Reading"];
+// console.log(hobbies[0]);
+// hobbies.push("Working")
+// console.log(hobbies)
+// 
+// let x = hobbies.findIndex((items) => items === "Cooking")
+// console.log(x)
+// const editedHobbies = hobbies.map((items) => ({text:items}));
+// console.log(editedHobbies)
+
+// const arr = ["Shubham","Shivam"]
+// firstName = arr[0];
+// secondName = arr[1]
+// const [firstName,secondName] = arr
+// console.log(firstName)
+// console.log(secondName)
+const user = {
+ name: "Shubham",
+ age:19
+}
+// 
+// const{name:userName,age} = user;
+// console.log(userName);
+// console.log(age);
+// 
+// const newHobbies = ["Reading"]
+// 
+// const mergeHobbies = [...hobbies,...newHobbies]
+// console.log(mergeHobbies)
+
+// const extendedUser = {
+//  isAdmin : true,
+//  ...user
+// }
+// console.log(extendedUser)
+
+// const password = prompt("Enter Password")
+// 
+// if(password === "Hello"){
+//  console.log("Hello works")
+// }else if(password === "hello"){
+//  console.log("hello works")
+// }else{
+//  console.log("Excess not granted.")
+// }
+// const hobbies = ["Singing","Reading","Cooking"]
+// 
+// for(const hobby of hobbies){
+//  console.log(hobby)
+// }
+// const list = document.selectQuery("ul")
+// list.remove();
+
+// function handleTimeout() {
+//  console.log("Timed out!");
+// }
+// 
+// const handleTimeout2 = () => {
+//  console.log("Timed out.. again!");
+// }
+// 
+// setTimeout(handleTimeout,3000)
+// setTimeout(handleTimeout2,5000)
+// setTimeout(() =>{
+//  console.log("More Time out!");
+// },7000);
+// function greeter(greetFn){
+//  greetFn();
+// }
+// greeter(() => console.log("hi"));
+
+// function init(){
+//  function greet(){
+//   console.log("hi");
+//  }
+//  greet();
+// }
+// init();
+
---
+++ JavaScriptRefresher/util.js
@@ -0,0 +1,3 @@
+export default "shubham"
+export let apiKey = "Shubham Maurya"
+export let api = "Shubh"
