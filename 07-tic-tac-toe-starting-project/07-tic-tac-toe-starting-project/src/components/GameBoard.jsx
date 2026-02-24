c965c2259e95647d8ab573ea1bdfe0fad64c877e
Its-Shubham_0246 <shubhammaurya2406@gmail.com>
2026-02-24 23:08:26 +0530

    learned how dynamical update the value in array used in useState

--- 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -1,3 +1,37 @@
-export default function GameBoard(){
- 
+import { useState } from 'react';
+
+const initialGameBoard = [
+ [null,null,null],
+ [null,null,null],
+ [null,null,null]
+]
+
+export default function GameBoard({onSelectSquare,currActivePlayer}){
+ const [gameBoard,setGameBoard] = useState(initialGameBoard);
+ function handleSelectSquare(rowIndex,colIndex){
+  setGameBoard(() => {
+   const updatedGameBoard = [...gameBoard.map((innerArray) => [...innerArray])];
+   updatedGameBoard[rowIndex][colIndex] = currActivePlayer;
+   return updatedGameBoard;
+  })
+  onSelectSquare();
+ }
+ return (
+  <ol id="game-board">
+   {
+    gameBoard.map((row,rowIndex) => (
+
+    <li key = {rowIndex}>
+     <ol>
+      {
+      row.map((playerSymbol,colIndex) => (
+        <li key = {colIndex}>
+         <button onClick = {() => handleSelectSquare(rowIndex,colIndex)}>{playerSymbol}</button>
+         </li>
+      ))}
+     </ol>
+    </li>)
+   )}
+  </ol>
+ )
 }
--- 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-export default function Player({initialName,symbol}){
+export default function Player({initialName,symbol,isActive}){
   const [playerName,setPlayerName] = useState(initialName);
   const [isEditing,setIsEditing] = useState(false);
   let editiblePlayerName = <span className="player-name">{playerName}</span>;;
@@ -13,7 +13,7 @@ export default function Player({initialName,symbol}){
     setIsEditing((editing) => !editing);
   }
   return (
-    <li>
+    <li className = {isActive  ? "active" : undefined}>
       <span className="player">
         {editiblePlayerName}
         <span className="player-symbol">{symbol}</span>
