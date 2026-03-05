3d24b6824e47c7c74e2cd3b99d05a419a66f71ac
Its-Shubham_0246 <shubhammaurya2406@gmail.com>
2026-03-05 11:59:19 +0530

    learned a concept that if we need to use a state that change occurs in multiple different components then define that state in the component where instances of the components used

--- 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,10 +1,21 @@
 import Player from './components/Player.jsx';
 import { useState,useEffect } from 'react';
 import GameBoard from './components/GameBoard.jsx';
+import Log from './components/Log.jsx';
 function App() {
   const [activePlayer,setActivePlayer] = useState("X");
-  function handleSelectSquare(){
+  const [gameTurns,setGameTurns] = useState([]);
+  function handleSelectSquare(rowIndex,colIndex){
     setActivePlayer((currActivePlayer) => (currActivePlayer === "X" ? "O" : "X"));
+    setGameTurns((prevTurns) => { 
+      let currentPlayer = 'X';
+      if(prevTurns.length > 0 && prevTurns[0].player === 'X'){
+        currentPlayer = 'O';
+      }
+      const updatedTurns = [{square: {row:rowIndex,col:colIndex},player : currentPlayer},...prevTurns];
+      return updatedTurns;
+    }
+    );
   }
   return (
     <main>
@@ -13,9 +24,9 @@ function App() {
             <Player initialName = "Player 1" symbol = "X" isActive = {activePlayer === "X"}/>
             <Player initialName = "Player 2" symbol = "O" isActive = {activePlayer === "O"} />
         </ol>
-        <GameBoard onSelectSquare = {handleSelectSquare} currActivePlayer={activePlayer} />
+        <GameBoard onSelectSquare = {handleSelectSquare} turns = {gameTurns} />
       </div>
-      log
+      <Log turns = {gameTurns} />
     </main>
   )
 }
--- 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
+++ 07-tic-tac-toe-starting-project/07-tic-tac-toe-starting-project/src/components/GameBoard.jsx
@@ -6,16 +6,22 @@ const initialGameBoard = [
  [null,null,null]
 ]
 
-export default function GameBoard({onSelectSquare,currActivePlayer}){
- const [gameBoard,setGameBoard] = useState(initialGameBoard);
- function handleSelectSquare(rowIndex,colIndex){
-  setGameBoard(() => {
-   const updatedGameBoard = [...gameBoard.map((innerArray) => [...innerArray])];
-   updatedGameBoard[rowIndex][colIndex] = currActivePlayer;
-   return updatedGameBoard;
-  })
-  onSelectSquare();
- }
+export default function GameBoard({onSelectSquare,turns}){
+//  const [gameBoard,setGameBoard] = useState(initialGameBoard);
+//  function handleSelectSquare(rowIndex,colIndex){
+//   setGameBoard(() => {
+//    const updatedGameBoard = [...gameBoard.map((innerArray) => [...innerArray])];
+//    updatedGameBoard[rowIndex][colIndex] = currActivePlayer;
+//    return updatedGameBoard;
+//   })
+//   onSelectSquare();
+//  }
+  let gameBoard = initialGameBoard;
+  for(const turn of turns){
+    const {square,player} = turn;
+    const {row,col} = square;
+    gameBoard[row][col] = player;
+  }
  return (
   <ol id="game-board">
    {
@@ -26,7 +32,7 @@ export default function GameBoard({onSelectSquare,currActivePlayer}){
       {
       row.map((playerSymbol,colIndex) => (
         <li key = {colIndex}>
-         <button onClick = {() => handleSelectSquare(rowIndex,colIndex)}>{playerSymbol}</button>
+         <button onClick = {gameBoard[rowIndex][colIndex] === null ? () => onSelectSquare(rowIndex,colIndex) : undefined}>{playerSymbol}</button>
          </li>
       ))}
      </ol>
