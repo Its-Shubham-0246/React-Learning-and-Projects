773aec423bd948fae365954c0fee23af37eb3887
Its-Shubham_0246 <shubhammaurya2406@gmail.com>
2026-03-27 23:47:46 +0530

    some changes

--- JavaScriptRefresher/tempCodeRunnerFile.js
+++
@@ -1,9 +0,0 @@
-var success = true;
-// 
-// let Promise = new Promise((resolve,reject) => {
-//  if(success){
-//   resolve("Data Connected");
-//  }else{
-//   reject("Error in Connection");
-//  }
-// });
